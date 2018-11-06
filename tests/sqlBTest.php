a72a67f1d5271f8e9d9589efa3833f3f6f3cd941
Henry Wong <hwong02@nyit.edu>
2018-11-06 11:24:37 -0500

    WIP: learning test unit

--- tests/sqlBTest.php
+++ tests/sqlBTest.php
@@ -2,6 +2,14 @@
 use PHPUnit\Framework\TestCase;
 
 final class sqlBTest extends TestCase {
+    // select * from table
+    // select col1 from table
+    // select col1, col2 from table
+    // select col1, col2 from table where col1 = col2
+    // select * from table where col1 = col2 and col2=col3
+    // select * from table where col1 = col2 or col2=col3
+    // select * fromt able where (col1 = col2 and col2=col3) or col4=col5
+    // select * fromt able where (col1 = col2 and col2=col3) and col4=col5
 }
 
 ?>
