a1d62b969324a9ff03b99004d3cb1ff826c92072
Henry Wong <hwong02@nyit.edu>
2018-11-26 15:59:44 -0500

    WIP

--- tests/sqlBTest.php
+++ tests/sqlBTest.php
@@ -1,6 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
-
+require __DIR__."/../vendor/autoload.php";
 final class sqlBTest extends TestCase {
     // select * from table
     // select col1 from table
@@ -10,6 +10,37 @@ final class sqlBTest extends TestCase {
     // select * from table where col1 = col2 or col2=col3
     // select * fromt able where (col1 = col2 and col2=col3) or col4=col5
     // select * fromt able where (col1 = col2 and col2=col3) and col4=col5
+
+    private $obj;
+
+    protected function setUp()
+    {
+        $this->obj = new NYIT\sqlB();
+    }
+
+    public function test1() {
+        // select * from table
+        $sql = $this->obj;
+        $sql->table('table');
+        $this->assertEquals('select * from table', $sql);
+    }
+
+    public function test2() {
+        // select col1 from table
+        $sql = $this->obj;
+        $sql->select('col1');
+        $sql->table('table1');
+        $this->assertEquals('select col1 from table1',$sql);
+    }
+
+    public function test3() {
+        // select col1, col2 from table
+        $sql = $this->obj;
+        $sql->select('col1, col2');
+        $sql->table('table1');
+        $this->assertEquals('select col1, col2 from table1',$sql);
+    }
+
 }
 
 ?>
